4c929ab04dce086cae7fad6706cd85c772b6c907
Matt Downey <SocuciusErgalla@users.noreply.github.com>
2017-09-08 09:54:46 -0400

    initial core library

---
+++ MIPS32_Sim_Core/MIPS32_Sim_Core/ArithmeticOperations.cs
@@ -0,0 +1,486 @@
+﻿
+using System;
+/// <summary>
+/// All arithmetic operations are defined here in alphabetical order
+/// </summary>
+namespace MIPS32_Sim_Core {
+
+    // ADD
+    public class Op_add : IOperation {
+        private int rs, rt, rd; // register refrences
+        private UInt32 lastRd; // state of dest register at begining of operation execution
+
+        public Op_add(int rs, int rt, int rd) {
+            this.rs = rs;
+            this.rt = rt;
+            this.rd = rd;
+        }
+
+        /// <summary>
+        /// Adds contents of rs to contents of rt and store in rd. Trap if overflow is detected.
+        /// Also saves the state of rd before changing it so it may be undone.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void doOp(MIPSChip_Instance chip) {
+            this.lastRd = chip.getRegister(this.rd);
+            try {
+                UInt32 temp = (UInt32)checked((Int32)chip.getRegister(rs) + (Int32)chip.getRegister(rt));
+                chip.setRegister(rd, temp);
+            } catch (OverflowException) {
+                chip.interrupt();
+            }
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reverts changes made by setting the contents of rd to the saved value.
+        /// For this to work properly, doOp must have been called only once with the same chip specified here.
+        /// Cannot account for any changes made to registers done by any trap handlers resulting from an overflow.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(this.rd, this.lastRd);
+        }
+    }
+
+    //ADDI
+    public class Op_addi : IOperation {
+        private int rs, rt; // register references
+        private short immediate; // signed 16bit value to add with
+        private UInt32 lastRt; // state of dest register at begining of operation execution
+        
+        public Op_addi(int rs, int rt, short immediate) {
+            this.rs = rs;
+            this.rt = rt;
+            this.immediate = immediate;
+        }
+
+        /// <summary>
+        /// Adds contents of rs to signed 16bit value and store in rt. Trap if overflow is detected.
+        /// Also saves the state of rt before changing it so it may be undone.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void doOp(MIPSChip_Instance chip) {
+            lastRt = chip.getRegister(rt);
+            try {
+                UInt32 temp = (UInt32)checked((Int32)chip.getRegister(rs) + (Int32)immediate);
+                chip.setRegister(rt, temp);
+            } catch (OverflowException) {
+                chip.interrupt();
+            }
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reverts changes made by setting the contents of rt to the saved value.
+        /// For this to work properly, doOp must have been called only once with the same chip specified here.
+        /// Cannot account for any changes made to registers done by any trap handlers resulting from an overflow.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(this.rt, this.lastRt);
+        }
+    }
+
+    //ADDIU
+    public class Op_addiu : IOperation {
+        private int rs, rt;
+        private short immediate; // register refrences
+        private UInt32 lastRt; // state of dest register at begining of operation execution
+
+        public Op_addiu(int rs, int rt, short immediate) {
+            this.rs = rs;
+            this.rt = rt;
+            this.immediate = immediate;
+        }
+
+        /// <summary>
+        /// Adds contents of rs to 16bit value and store in rt. Does not do anything upon overflow.
+        /// Also saves the state of rt before changing it so it may be undone.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void doOp(MIPSChip_Instance chip) {
+            lastRt = chip.getRegister(rt);
+            chip.setRegister(rt, unchecked(chip.getRegister(rs) + (UInt32)immediate));
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reverts changes made by setting the contents of rt to the saved value.
+        /// For this to work properly, doOp must have been called only once with the same chip specified here.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(this.rt, this.lastRt);
+        }
+    }
+
+    //ADDU
+    public class Op_addu : IOperation {
+        private int rs, rt, rd; // register refrences
+        private UInt32 lastRd; // state of dest register at begining of operation execution
+
+        public Op_addu(int rs, int rt, int rd) {
+            this.rs = rs;
+            this.rt = rt;
+            this.rd = rd;
+        }
+
+        /// <summary>
+        /// Adds contents of rs to contents of rt and store in rd.
+        /// Also saves the state of rd before changing it so it may be undone.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void doOp(MIPSChip_Instance chip) {
+            this.lastRd = chip.getRegister(this.rd);
+            chip.setRegister(rd, unchecked(chip.getRegister(rs) + chip.getRegister(rt)));
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reverts changes made by setting the contents of rd to the saved value.
+        /// For this to work properly, doOp must have been called only once with the same chip specified here.
+        /// </summary>
+        /// <param name="chip"></param>
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(this.rd, this.lastRd);
+        }
+    }
+
+    //CLO
+    public class Op_clo : IOperation {
+        private int rs, rd;
+        private UInt32 last;
+
+        public Op_clo(int rs, int rd) {
+            this.rs = rs;
+            this.rd = rd;
+        }
+
+        public void doOp(MIPSChip_Instance chip) {
+            this.last = chip.getRegister(rd);
+            int count = 32;
+            UInt32 value = chip.getRegister(rs);
+            for (int i = 31; i >= 0; i--) {
+                if (value >> i != 1) {
+                    count = 31 - i;
+                    break;
+                }
+            }
+            chip.setRegister(rd, (UInt32)count);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(rd, last);
+        }
+    }
+
+    //CLZ
+    public class Op_clz : IOperation {
+        private int rs, rd;
+        private UInt32 last;
+
+        public Op_clz(int rs, int rd) {
+            this.rs = rs;
+            this.rd = rd;
+        }
+
+        public void doOp(MIPSChip_Instance chip) {
+            this.last = chip.getRegister(rd);
+            int count = 32;
+            UInt32 value = chip.getRegister(rs);
+            for (int i = 31; i >= 0; i--) {
+                if (value >> i == 1) {
+                    count = 31 - i;
+                    break;
+                }
+            }
+            chip.setRegister(rd, (UInt32)count);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(rd, last);
+        }
+    }
+
+    //DIV
+    public class Op_div : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_div(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            chip.HI = (UInt32)unchecked((Int32)chip.getRegister(rs) / (Int32)chip.getRegister(rt));
+            chip.LO = (UInt32)unchecked((Int32)chip.getRegister(rs) % (Int32)chip.getRegister(rt));
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+
+    //DIVU
+    public class Op_divu : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_divu(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            chip.HI = (UInt32)((Int32)unchecked(chip.getRegister(rs) % chip.getRegister(rt)));
+            chip.LO = (UInt32)((Int32)unchecked(chip.getRegister(rs) / chip.getRegister(rt)));
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+
+    //MADD
+    public class Op_madd : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_madd(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            Int64 result = (Int64)((UInt64)(lastHi << 32) | (UInt64)lastLo);
+            result += unchecked((Int64)chip.getRegister(rs) * (Int64)chip.getRegister(rt));
+            
+            chip.HI = (UInt32)(result >> 32);
+            chip.LO = (UInt32)(result & 0x0000FFFF);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+
+    //MADDU
+    public class Op_maddu : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_maddu(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            UInt64 result = (UInt64)(lastHi << 32) | (UInt64)lastLo;
+            result += unchecked((UInt64)chip.getRegister(rs) * (UInt64)chip.getRegister(rt));
+
+            chip.HI = (UInt32)(result >> 32);
+            chip.LO = (UInt32)(result & 0x0000FFFF);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+    //public class Op_maddu : IOperation {
+    //    private int rs, rt;
+    //    private UInt32 lastHi, lastLo;
+    //    public Op_maddu(int rs, int rt) {
+    //        this.rs = rs;
+    //        this.rt = rt;
+    //    }
+    //    public void doOp(MIPSChip_Instance chip) {
+    //        lastHi = chip.HI;
+    //        lastLo = chip.LO;
+    //        UInt64 result = unchecked((UInt64)chip.getRegister(rs) * (UInt64)chip.getRegister(rt));
+    //        chip.HI = (UInt32)(result >> 32);
+    //        chip.LO = (UInt32)(result & 0x0000FFFF);
+    //    }
+
+    //    public uint getByteCode() {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void undoOp(MIPSChip_Instance chip) {
+    //        chip.HI = lastHi;
+    //        chip.LO = lastLo;
+    //    }
+    //}
+
+    //MSUB
+    public class Op_msub : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_msub(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            Int64 result = (Int64)((UInt64)(lastHi << 32) | (UInt64)lastLo);
+            result -= unchecked((Int64)chip.getRegister(rs) * (Int64)chip.getRegister(rt));
+
+            chip.HI = (UInt32)(result >> 32);
+            chip.LO = (UInt32)(result & 0x0000FFFF);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+
+    //MSUBU
+    public class Op_msubu : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_msubu(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            UInt64 result = (UInt64)(lastHi << 32) | (UInt64)lastLo;
+            result -= unchecked((UInt64)chip.getRegister(rs) * (UInt64)chip.getRegister(rt));
+
+            chip.HI = (UInt32)(result >> 32);
+            chip.LO = (UInt32)(result & 0x0000FFFF);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+
+    //MUL
+    public class Op_mul : IOperation {
+        private int rs, rt, rd;
+        private UInt32 last;
+        public Op_mul(int rs, int rt, int rd) {
+            this.rs = rs;
+            this.rt = rt;
+            this.rd = rd;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            last = chip.getRegister(rd);
+            Int64 result = unchecked((Int64)chip.getRegister(rs) * (Int64)chip.getRegister(rd));
+            chip.setRegister(rd, (UInt32)(result & 0x0000ffff));
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.setRegister(rd, last);
+        }
+    }
+
+    //MULT
+    public class Op_mult : IOperation {
+        private int rs, rt;
+        private UInt32 lastHi, lastLo;
+        public Op_mult(int rs, int rt) {
+            this.rs = rs;
+            this.rt = rt;
+        }
+        public void doOp(MIPSChip_Instance chip) {
+            lastHi = chip.HI;
+            lastLo = chip.LO;
+            Int64 result = unchecked((Int64)chip.getRegister(rs) * (Int64)chip.getRegister(rt));
+            chip.HI = (UInt32)(result >> 32);
+            chip.LO = (UInt32)(result & 0x0000FFFF);
+        }
+
+        public uint getByteCode() {
+            throw new NotImplementedException();
+        }
+
+        public void undoOp(MIPSChip_Instance chip) {
+            chip.HI = lastHi;
+            chip.LO = lastLo;
+        }
+    }
+    //MULTU
+    //SEB
+    //SEH
+    //SLT
+    //SLTI
+    //SLTIU
+    //SLTU
+    //SUB
+    //SUBU
+}
---
+++ MIPS32_Sim_Core/MIPS32_Sim_Core/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIPS32_Sim_Core {
+
+    /// <summary>
+    /// Class to represent an "instance" of a MIPS32 chip that operations are performed on
+    /// </summary>
+    public class MIPSChip_Instance {
+        private UInt32[] registers;
+        private UInt32 hi;
+        private UInt32 lo;
+
+        public UInt32 HI {
+            get { return this.hi; }
+            set { this.hi = value; }
+        }
+        public UInt32 LO {
+            get { return this.lo; }
+            set { this.lo = value; }
+        }
+
+        /// <summary>
+        /// Returns the value contained in the specified register
+        /// </summary>
+        /// <param name="reg">Which register to grab the value from</param>
+        /// <returns></returns>
+        public UInt32 getRegister(int reg) {
+            return registers[reg];
+        }
+
+        /// <summary>
+        /// Forces the specified register to have the specified value
+        /// </summary>
+        /// <param name="reg"></param>
+        /// <param name="value"></param>
+        internal void setRegister(int reg, UInt32 value) {
+            this.registers[reg] = value;
+        }
+
+        /// <summary>
+        /// signal an interrupt TODO
+        /// </summary>
+        internal void interrupt() {
+            //TODO
+        }
+    }
+
+    /// <summary>
+    /// Describes any MIPS operation that can be performed on MIPSChip_Instance
+    /// </summary>
+    public interface IOperation {
+        void doOp(MIPSChip_Instance chip);
+        void undoOp(MIPSChip_Instance chip);
+        UInt32 getByteCode();
+        //String getName();
+        //String getFormat();
+        //String getDesc();
+    }
+
+}
